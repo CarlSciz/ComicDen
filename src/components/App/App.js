6d132d7664eb3366e1ccb9bcbb16b94fc227e783
CarlSciz <46180364+CarlSciz@users.noreply.github.com>
2023-10-29 23:06:22 -0400

    src: installed router-dom, added two new routes
    Created Two routes. Currently work in progress.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import comicDen from '../ComicDen/comicDen';
+import Preferences from '../Preferences/preferences';
 
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/comicDen" element={<comicDen />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ComicDen/comicDen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function comicDen() {
+    return(
+        <h2>ComicDen</h2>
+    );
+}
