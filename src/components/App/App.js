c3eebf6e71731a8dd693ccade0e5094457a2d05f
CarlSciz <46180364+CarlSciz@users.noreply.github.com>
2023-11-02 23:49:29 -0400

    Creating Auth Token and login
    installed express, developing login functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import comicDen from '../ComicDen/comicDen';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ComicDen from '../ComicDen/comicden';
+import Login from '../Login/login';
 import Preferences from '../Preferences/preferences';
 
+
 function App() {
+  
+  const [token, setToken] = useState();
+
+  if (token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/comicDen" element={<comicDen />} />
+          <Route path="/comicDen" element={<ComicDen />} />
           <Route path="/preferences" element={<Preferences />} />
         </Routes>
       </BrowserRouter>
